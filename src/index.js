e9e59c28b40d53a80fcfb14a5fd6e14156151bd4
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2023-08-18 19:00:12 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let stack = [];
+    let openBrackets = [];
+    let closeBrackets = [];
+    let sameBrackets = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+            sameBrackets.push(bracketsConfig[i][0]);
+        } else {
+            openBrackets.push(bracketsConfig[i][0]);
+            closeBrackets.push(bracketsConfig[i][1]);
+        }
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (sameBrackets.includes(str[i])) {
+            if (stack[stack.length - 1] === str[i]) {
+                stack.pop();
+            } else {
+                stack.push(str[i]);
+            }
+        } else if (openBrackets.includes(str[i])) {
+            stack.push(str[i]);
+        } else if (closeBrackets.includes(str[i])) {
+            if (
+                stack.length === 0 ||
+                openBrackets.indexOf(stack.pop()) !== closeBrackets.indexOf(str[i])
+            ) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
